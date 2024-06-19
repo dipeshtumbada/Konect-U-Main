2ed411d1bb7ce2f20db5fa5d361db388b6bf86bf
Jess John <jessjohn1539@gmail.com>
2024-06-20 00:45:57 +0530

    bg colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ export default {
         'primary': '#FB5909',
         'buttons': '#FEB702',
         'bg': '#FFF5EA',
-        'gradient': 'background: linear-gradient(to right, #fb4d08 0%,#ffb703 80%,#ffffff 100%)',
-        'gradient1': 'background: linear-gradient(to right, #fb4d08 0%,#ffb703 50%,#fb4d08 100%)',
-        'rad-gradient': 'background: radial-gradient(ellipse at center, #ffb703 0%,#ff9325 54%,#fb4d08 100%)',
-      }
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(to right, #fb4d08 0%,#ffb703 80%,#ffffff 100%)',
+        'gradient1': 'linear-gradient(to right, #fb4d08 0%,#ffb703 50%,#fb4d08 100%)',
+        'radgradient': 'radial-gradient(ellipse at center, #ffb703 0%,#ff9325 54%,#fb4d08 100%)',
+      },
     },
   },
   plugins: [],
