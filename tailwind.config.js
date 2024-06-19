558f13415f6987ec1c19b269ee7ea1671836ebd2
Jess John <jessjohn1539@gmail.com>
2024-06-19 21:30:14 +0530

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-bg': '#161921',
+        'primary': '#FB5909',
+        'buttons': '#FEB702',
+        'bg': '#FFF5EA',
+        'gradient': 'background: linear-gradient(to right, #fb4d08 0%,#ffb703 80%,#ffffff 100%)',
+        'gradient1': 'background: linear-gradient(to right, #fb4d08 0%,#ffb703 50%,#fb4d08 100%)',
+        'rad-gradient': 'background: radial-gradient(ellipse at center, #ffb703 0%,#ff9325 54%,#fb4d08 100%)',
+      }
+    },
   },
   plugins: [],
 }
