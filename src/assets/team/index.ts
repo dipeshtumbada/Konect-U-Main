1a9c87f5fd29da41d99605a5cd40b7b13b934504
Jess John <jessjohn1539@gmail.com>
2024-06-20 20:05:57 +0530

    Team added

---
+++ src/assets/team/index.ts
@@ -0,0 +1,13 @@
+import clive from './clive.jpg';
+import kundeshwar from './kundeshwar.jpg';
+import kshitij from './kshitij.jpg';
+import mauli from './mauli.jpg';
+import kevin from './kevin.jpg';
+
+export {
+    clive,
+    kundeshwar,
+    kshitij,
+    mauli,
+    kevin
+}
