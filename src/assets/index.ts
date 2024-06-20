be6bb4d0041828717e8cad46b78659b846760e20
Jess John <jessjohn1539@gmail.com>
2024-06-20 16:08:30 +0530

    Testimonials added

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import People01 from "./universities/chicago.png";
+import People02 from "./universities/chicago.png";
+import People03 from "./universities/chicago.png";
+
+export { People01, People02, People03 };
---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+
+import { people01, people02, people03} from "../assets";
+
+export const feedback = [
+    {
+      id: "feedback-1",
+      content:
+        "After 4 years as a software engineer, I pursued a Master's in Data Science using the Nalanda AI model from Konect U. Highly recommend Konect U!",
+      name: "Herman Jensen",
+      title: "MBA Grad - John Hopkins University",
+      img: people01,
+    },
+    {
+      id: "feedback-2",
+      content:
+        "After 4 years as a software engineer, I pursued a Master's in Data Science using the Nalanda AI model from Konect U. Highly recommend Konect U!",
+      name: "Steve Mark",
+      title: "CS Grad - Harvard University",
+      img: people02,
+    },
+    {
+      id: "feedback-3",
+      content:
+        "After 4 years as a software engineer, I pursued a Master's in Data Science using the Nalanda AI model from Konect U. Highly recommend Konect U!",
+      name: "Kenn Gallagher",
+      title: "EE Grad - Berkeley University",
+      img: people03,
+    },
+  ];
