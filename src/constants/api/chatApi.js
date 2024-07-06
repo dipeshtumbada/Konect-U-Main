d62bd89c965bc3b79b325f69017b73f78ed6b3e9
dipeshtumbada <134152238+dipeshtumbada@users.noreply.github.com>
2024-07-06 23:15:44 +0530

    Update chatApi.js

--- src/constants/api/chatApi.js
+++ src/constants/api/chatApi.js
@@ -10,7 +10,7 @@ const port = process.env.GROQ_PORT || 5000; // Use GROQ_PORT from environment va
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 app.use(cors({
-  origin: 'http://54.164.186.90',
+  origin: 'http://http://100.26.156.106',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
